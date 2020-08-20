f8bd61946dc9acd24aa15ae47cb0ee35608b9a41
Sebastian Montoya <spartanmayhem@gmail.com>
2020-08-20 17:56:32 +0200

    Working on populateField()

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,13 +2,13 @@
 
 const game = 64; //Tamaño del campo
 const set = 16; //dificultad
-var bombSet = [];
+let bombSet = [];
 
 //genera las bombas del juego en un array aleatorio
 //comprobando si existe con cada iteracion
 function bombGenerator(game){
 
-    for (var i = 0; i < set; i++){ 
+    for (let i = 0; i < set; i++){ 
         
         var bomb = Math.floor(Math.random() * game);
         
@@ -27,6 +27,45 @@ function bombGenerator(game){
 
 // colocar las bombas en sus casillas
 
+function populateField() {
+    var isBomb;
+    var bombCell, emptyCell;
+    var tableArr = [];
+
+    console.log(bombSet.find(isBomb => isBomb == 0));
+    if (bombSet.find(isBomb => isBomb == 0) == 0){
+        tableArr.push("BOMB");
+    }else 
+    tableArr.push("NOPE");
+    
+
+    //     for(let i = 0; i < game; i++){
+    //     if ( i == bombSet.find(isBomb => isBomb == i)) {
+    //     bombCell = `<li id="${i}">*</li>`;
+    //     tableArr.push(bombCell);
+    //     }else        
+    //     emptyCell = `<li id="${i}"></li>`;
+     
+    //     tableArr.push(emptyCell);
+    // }
+    
+   return tableArr;
+}
+
+
+
+function createTable(){
+    bombGenerator(game);
+    theGame = populateField();
+
+    for (let i = 0; i < set; i++){
+    
+    }
+    return theGame;
+}
+
+
 console.log(bombGenerator(game));
 
-console.log(bombSet.length);
+
+console.log(createTable());
