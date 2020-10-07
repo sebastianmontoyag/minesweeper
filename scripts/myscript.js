b3a0557cd5fc4318dac30c67385f3d68a91a81d9
Sebastian Montoya <spartanmayhem@gmail.com>
2020-10-07 21:34:14 +0200

    implemeting my logic to the code to prevent cheating

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
+    const reset = document.getElementById('reset');
     let width = 10;
     let bombAmount = 20;
     let flags = 0;
@@ -10,19 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
     //create board
     function createBoard() {
         //create game with random bombs
-        const bombsArray = Array(bombAmount).fill('bomb');
-        const emptyArray = Array(width*width - bombAmount).fill('valid'); 
-        const gameArray = emptyArray.concat(bombsArray);
-        const shuffledArray = gameArray.sort(() => Math.random() -0.5);
-
+        //const bombsArray = Array(bombAmount).fill('bomb');
+        const bombsArray = generateBombs(width*width);
+        // const emptyArray = Array(width*width - bombAmount).fill('valid'); 
+        // const gameArray = emptyArray.concat(bombsArray);
+        // const shuffledArray = gameArray.sort(() => Math.random() -0.5);
+        
+        console.log(bombsArray);
+        
 
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div');
             square.setAttribute('id', i);
-            square.classList.add(shuffledArray[i]);
+            // square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
-
+            
             //normal click
             square.addEventListener('click', function(e){
                 click(square);
@@ -35,13 +39,18 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
+        // test: check current square in the array
+            var currrent = squares[parseInt(10)].id; 
+            console.log(currrent);
+            console.log(bombsArray.includes(parseInt(currrent)));
         //add numbers
         for (let i = 0; i < squares.length; i++){
             let total = 0;
             const isLeftEdge = (i % width === 0);
             const isRightEdge = (i % width === width -1);
 
-            if (squares[i].classList.contains('valid')) {
+            // if (squares[i].classList.contains('valid')) {
+            if (bombsArray.includes(parseInt(i))) {
                 if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++;
                 if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total++;
                 if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
@@ -180,5 +189,28 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
+
+    //reset game
+    reset.onclick = function() {
+        window.location.reload(); 
+    }
+
+    //generdor de bombas
+    function generateBombs(game){
+        var bombSet = [];
+    for (let i = 0; i < bombAmount; i++){ 
+        
+        var bomb = Math.floor(Math.random() * game);
+
+        if (bombSet.includes(bomb)){
+            bomb = Math.floor(Math.random() * game);
+            bombSet.push(bomb);
+
+        }else
+        bombSet.push(bomb);
+
+    }    
+    return bombSet;
+}
 
 })
