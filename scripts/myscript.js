4862a7c552d65951942ce01c2214520d5d4e3d19
Sebastian Montoya <spartanmayhem@gmail.com>
2020-10-08 16:36:42 +0200

    added my logic, bomb counter, now is harder to cheat

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,29 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const reset = document.getElementById('reset');
+    const bombs = document.getElementById('bombsLeft');
     let width = 10;
     let bombAmount = 20;
     let flags = 0;
     let squares = [];
     let isGameOver = false;
-
+    let bombsArray = [];
+    let numArray = [];
 
     //create board
     function createBoard() {
         //create game with random bombs
-        //const bombsArray = Array(bombAmount).fill('bomb');
-        const bombsArray = generateBombs(width*width);
-        // const emptyArray = Array(width*width - bombAmount).fill('valid'); 
-        // const gameArray = emptyArray.concat(bombsArray);
-        // const shuffledArray = gameArray.sort(() => Math.random() -0.5);
-        
-        console.log(bombsArray);
-        
-
+        bombsArray = generateBombs(width*width);
+        bombs.innerHTML = bombAmount - flags;
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div');
             square.setAttribute('id', i);
-            // square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
             
@@ -39,39 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        // test: check current square in the array
-            var currrent = squares[parseInt(10)].id; 
-            console.log(currrent);
-            console.log(bombsArray.includes(parseInt(currrent)));
+
         //add numbers
         for (let i = 0; i < squares.length; i++){
             let total = 0;
             const isLeftEdge = (i % width === 0);
             const isRightEdge = (i % width === width -1);
 
-            // if (squares[i].classList.contains('valid')) {
-            if (bombsArray.includes(parseInt(i))) {
-                if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++;
-                if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total++;
-                if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
-                if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total++;
-                if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total++;
-                if (i < 90 && !isLeftEdge && squares[i -1 +width].classList.contains('bomb')) total++;
-                if (i < 88 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total++;
-                if (i < 89 && squares[i +width].classList.contains('bomb')) total++;
-                squares[i].setAttribute('data', total);
-                
+            if (!bombsArray.includes(parseInt(i))) {
+                if (i > 0 && !isLeftEdge && bombsArray.includes(parseInt(i - 1))) total ++;
+                if (i > 9 && !isRightEdge && bombsArray.includes(parseInt(i + 1 - width))) total++;
+                if (i > 10 && bombsArray.includes(parseInt(i - width))) total++;
+                if (i > 11 && !isLeftEdge && bombsArray.includes(parseInt(i - 1 - width))) total++;
+                if (i < 98 && !isRightEdge && bombsArray.includes(parseInt(i + 1))) total++;
+                if (i < 90 && !isLeftEdge && bombsArray.includes(parseInt(i - 1 + width))) total++;
+                if (i < 88 && !isRightEdge && bombsArray.includes(parseInt(i + 1 + width))) total++;
+                if (i < 89 && bombsArray.includes(parseInt(i + width))) total++;
+                numArray[i] = total;
                 
              
             }
         }
-
+            
 
 
     }
 
     createBoard();
-
     //add flag
     function addFlag(square) {
         if (isGameOver) return;
@@ -80,11 +68,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 square.classList.add('flag');
                 square.innerHTML = '🚩';
                 flags++;
-                checkForWin();
+                checkForWin();                
+                bombs.innerHTML = bombAmount - flags;
             }else{
                 square.classList.remove('flag');
-                square.innerHtml = '';
-                flaggs--;
+                square.innerHTML = '';
+                flags--;
+                bombs.innerHTML = bombAmount - flags;
             }
         }
     }
@@ -94,10 +84,12 @@ document.addEventListener('DOMContentLoaded', () => {
         let currentId = square.id;
         if (isGameOver) return;
         if (square.classList.contains('checked') || square.classList.contains('flag')) return;
-            if (square.classList.contains('bomb')) {
+            if (bombsArray.includes(parseInt(currentId))){
             gameOver(square);
         }else {
-            let total = square.getAttribute('data')
+            
+            let total = numArray[parseInt(currentId)];
+            
             if (total !=0) {
                 square.classList.add('checked');
                 square.innerHTML = total;
@@ -109,11 +101,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    //check neighboring squaren once clicked
+    //check neighboring square once clicked
     function checkSquare(square, currentId) {
-        const isLeftEdge = (currentId % width ===0);
+        const isLeftEdge = (currentId % width === 0);
         const isRightEdge = (currentId % width === width -1);
-
+        
         setTimeout(() => {
             if (currentId > 0 && !isLeftEdge) {
                 const newId = squares[parseInt(currentId) - 1].id;
@@ -168,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         //show all bombs
         squares.forEach(square => {
-            if (square.classList.contains('bomb')) {
+            if (bombsArray.includes(parseInt(square.id))) {
                 square.innerHTML = '💣';
             }
         })
@@ -179,7 +171,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function checkForWin() {
         let matches = 0;
         for (let i = 0; i < squares.length; i++) {
-            if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
+            if (squares[i].classList.contains('flag') && bombsArray.includes(parseInt(i))) {
                 matches++;
             }
 
@@ -195,7 +187,7 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.reload(); 
     }
 
-    //generdor de bombas
+    //geneate the bombs
     function generateBombs(game){
         var bombSet = [];
     for (let i = 0; i < bombAmount; i++){ 
